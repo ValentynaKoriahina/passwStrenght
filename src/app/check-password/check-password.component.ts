ce679503fccbdd0f39b539421926b5445db18fc0
Velentyna Koriahina <v.koriagina.work@gmail.com>
2024-03-05 18:39:02 +0000

    Added styles
    Applying styles depending on the complexity of the entered password

--- src/app/check-password/check-password.component.ts
+++ src/app/check-password/check-password.component.ts
@@ -1,45 +1,65 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-check-password',
   templateUrl: './check-password.component.html',
   styleUrls: ['./check-password.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class CheckPasswordComponent {
   password: string = '';
+  classWeak: string = 'undefined-pass';
+  classMedium: string = 'undefined-pass';
+  classStrong: string = 'undefined-pass';
 
-  logPassword(value: string) {
-    console.log(value.split(''));
-  }
 
   ngDoCheck() {
-    var regExS = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)(?=.*[\p{P}\p{S}]).+$/u;
-    var regExM = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)|(?=.*[a-zA-Zа-яА-Я])(?=.*[\p{P}\p{S}])|(?=.*\d)(?=.*[\p{P}\p{S}])[a-zA-Zа-яА-Я\d\p{P}\p{S}]+$/u;
+    var regExStrong = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)(?=.*[\p{P}\p{S}]).+$/u;
+    var regExMedium = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)|(?=.*[a-zA-Zа-яА-Я])(?=.*[\p{P}\p{S}])|(?=.*\d)(?=.*[\p{P}\p{S}])[a-zA-Zа-яА-Я\d\p{P}\p{S}]+$/u;
 
     console.log(this.password);
 
-    if (this.password.length < 8) {
+    if (this.password.length === 0) {
 
-      console.log("Пароль не соответствует требованиям  безопасности.");
+      this.classWeak = 'undefined-pass';
+      this.classMedium = 'undefined-pass';
+      this.classStrong = 'undefined-pass';
+
+
+      console.log("Пароль не введен.");
+
+    } else if (this.password.length < 8) {
+        this.classWeak = 'weak-pass';
+        this.classMedium = 'weak-pass';
+        this.classStrong = 'weak-pass';
+
+        console.log("Пароль не соответствует требованиям  безопасности.");
 
     } else {
 
 
-      if (regExS.test(this.password)) {
+      if (regExStrong.test(this.password)) {
+        this.classWeak = 'strong-pass';
+        this.classMedium = 'strong-pass';
+        this.classStrong = 'strong-pass';
 
         console.log("Пароль является сильным.");
 
-      } else if (regExM.test(this.password)) {
+      } else if (regExMedium.test(this.password)) {
+        this.classWeak = 'medium-pass';
+        this.classMedium = 'medium-pass';
+        this.classStrong = 'undefined-pass';
 
         console.log("Пароль является средним.");
 
       } else {
-
-        console.log("Пароль является ленким.");
+        this.classWeak = 'weak-pass';
+        this.classMedium = 'undefined-pass';
+        this.classStrong = 'undefined-pass';
+        console.log("Пароль является легким.");
 
       }
     }
