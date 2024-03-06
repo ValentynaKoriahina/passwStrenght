11efff1a428f1de5422cf639795079601f8d7de0
Velentyna Koriahina <v.koriagina.work@gmail.com>
2024-03-06 23:38:32 +0000

    Adding form styles and password validation function

--- src/app/check-password/check-password.component.ts
+++ src/app/check-password/check-password.component.ts
@@ -16,51 +16,31 @@ export class CheckPasswordComponent {
   classStrong: string = 'undefined-pass';
 
 
-  ngDoCheck() {
+  checkPassword() {
     var regExStrong = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)(?=.*[\p{P}\p{S}]).+$/u;
     var regExMedium = /^(?=.*[a-zA-Zа-яА-Я])(?=.*\d)|(?=.*[a-zA-Zа-яА-Я])(?=.*[\p{P}\p{S}])|(?=.*\d)(?=.*[\p{P}\p{S}])[a-zA-Zа-яА-Я\d\p{P}\p{S}]+$/u;
 
-    console.log(this.password);
-
     if (this.password.length === 0) {
-
       this.classWeak = 'undefined-pass';
       this.classMedium = 'undefined-pass';
       this.classStrong = 'undefined-pass';
-
-
-      console.log("Пароль не введен.");
-
     } else if (this.password.length < 8) {
-        this.classWeak = 'weak-pass';
-        this.classMedium = 'weak-pass';
-        this.classStrong = 'weak-pass';
-
-        console.log("Пароль не соответствует требованиям  безопасности.");
-
+      this.classWeak = 'weak-pass';
+      this.classMedium = 'weak-pass';
+      this.classStrong = 'weak-pass';
     } else {
-
-
       if (regExStrong.test(this.password)) {
         this.classWeak = 'strong-pass';
         this.classMedium = 'strong-pass';
         this.classStrong = 'strong-pass';
-
-        console.log("Пароль является сильным.");
-
       } else if (regExMedium.test(this.password)) {
         this.classWeak = 'medium-pass';
         this.classMedium = 'medium-pass';
         this.classStrong = 'undefined-pass';
-
-        console.log("Пароль является средним.");
-
       } else {
         this.classWeak = 'weak-pass';
         this.classMedium = 'undefined-pass';
         this.classStrong = 'undefined-pass';
-        console.log("Пароль является легким.");
-
       }
     }
   }
